41b214c4dffa398c3287464b29fff8bb625f23e7
Aleksandar <saksofon997@gmail.com>
2021-09-01 23:37:07 +0200

    Pack app as package

---
+++ springr_generator/generator.py
@@ -0,0 +1,246 @@
+from os import makedirs
+from os.path import dirname, join
+import glob
+import jinja2
+from springr_generator.model import get_entity_mm
+from datetime import datetime
+from textx import generator
+
+
+def generate_folders(root):
+    output_root = root + "/src/main/"
+    package = "jsd/tim/"
+    folder_structure = "java/" + package
+
+    packages = [
+        folder_structure+"model",
+        folder_structure+"dto",
+        folder_structure+"repository",
+        folder_structure+"controller",
+        folder_structure+"service",
+        folder_structure+"service/impl",
+        "resources",
+        "webapp/WEB-INF/jsp"
+    ]
+
+    # Create output folder structure
+    [makedirs(f"{output_root}/{package}", exist_ok=True)
+     for package in packages]
+
+    return folder_structure
+
+
+def app_generator(model_path, output_root, project_name):
+    this_folder = dirname(__file__)
+
+    entity_mm = get_entity_mm()
+
+    # Build model from file
+    test_model = entity_mm.model_from_file(model_path)
+
+    def javatype(s):
+        """
+        Maps type names from PrimitiveType to Java.
+        """
+        return {
+            'long': 'long',
+            'integer': 'int',
+            'string': 'String',
+            'bool': 'boolean',
+        }.get(s.name, s.name)
+
+    jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(this_folder),
+        trim_blocks=True,
+        lstrip_blocks=True)
+
+    jinja_env.filters['javatype'] = javatype
+
+    folder_structure = generate_folders(output_root)
+
+    print("Generating app... Output folder: " + output_root +
+          " Folder structure: " + folder_structure)
+
+    for template in glob.glob("templates/*.template"):
+        # Load a template
+        jinja_template = jinja_env.get_template(template.replace("\\", "/"))
+
+        if "model" in template:
+            for entity in test_model.entities:
+                shared = [
+                    x for x in test_model.shared.entities if x.name == entity.name]
+
+                if shared:
+                    with open(join(f"{output_root}{folder_structure}/model", f"{entity.name.capitalize()}.java"), 'w') as f:
+                        f.write(jinja_template.render(entity=entity,
+                                shared=test_model.shared, time=datetime.now()))
+                else:
+                    with open(join(f"{output_root}{folder_structure}/model", f"{entity.name.capitalize()}.java"), 'w') as f:
+                        f.write(jinja_template.render(
+                            entity=entity, time=datetime.now()))
+
+        if "controller" in template:
+            for entity in test_model.entities:
+                shared = [
+                    x for x in test_model.shared.entities if x.name == entity.name]
+
+                if shared:
+                    with open(join(f"{output_root}{folder_structure}/controller", f"{entity.name.capitalize()}Controller.java"), 'w') as f:
+                        f.write(jinja_template.render(entity=entity,
+                                shared=test_model.shared, time=datetime.now()))
+                else:
+                    with open(join(f"{output_root}{folder_structure}/controller", f"{entity.name.capitalize()}Controller.java"), 'w') as f:
+                        f.write(jinja_template.render(
+                            entity=entity, time=datetime.now()))
+
+        if "repository" in template:
+            for entity in test_model.entities:
+                # For each entity generate file
+                with open(join(f"{output_root}{folder_structure}/repository", f"{entity.name.capitalize()}Repository.java"), 'w') as f:
+                    f.write(jinja_template.render(
+                        entity=entity, time=datetime.now()))
+
+        if "dto" in template:
+            for entity in test_model.entities:
+                shared = [
+                    x for x in test_model.shared.entities if x.name == entity.name]
+
+                if shared:
+                    with open(join(f"{output_root}{folder_structure}/dto", f"{entity.name.capitalize()}DTO.java"), 'w') as f:
+                        f.write(jinja_template.render(entity=entity,
+                                shared=test_model.shared, time=datetime.now()))
+                else:
+                    with open(join(f"{output_root}{folder_structure}/dto", f"{entity.name.capitalize()}DTO.java"), 'w') as f:
+                        f.write(jinja_template.render(
+                            entity=entity, time=datetime.now()))
+
+        if "service" in template:
+            for entity in test_model.entities:
+                shared = [
+                    x for x in test_model.shared.entities if x.name == entity.name]
+
+                if shared:
+                    with open(join(f"{output_root}{folder_structure}/service", f"{entity.name.capitalize()}Service.java"), 'w') as f:
+                        f.write(jinja_template.render(entity=entity,
+                                shared=test_model.shared, time=datetime.now()))
+                else:
+                    with open(join(f"{output_root}{folder_structure}/service", f"{entity.name.capitalize()}Service.java"), 'w') as f:
+                        f.write(jinja_template.render(
+                            entity=entity, time=datetime.now()))
+
+        if "implService" in template:
+            for entity in test_model.entities:
+                shared = [
+                    x for x in test_model.shared.entities if x.name == entity.name]
+
+                if shared:
+                    with open(join(f"{output_root}{folder_structure}/service/impl", f"{entity.name.capitalize()}ServiceImpl.java"), 'w') as f:
+                        f.write(jinja_template.render(entity=entity,
+                                shared=test_model.shared, time=datetime.now()))
+                else:
+                    with open(join(f"{output_root}{folder_structure}/service/impl", f"{entity.name.capitalize()}ServiceImpl.java"), 'w') as f:
+                        f.write(jinja_template.render(
+                            entity=entity, time=datetime.now()))
+
+        if "appProperties" in template:
+            for entity in test_model.entities:
+                with open(join(f"{output_root}/resources", f"application.properties"), 'w') as f:
+                    f.write(jinja_template.render(entity=entity,
+                            projectName=project_name, time=datetime.now()))
+
+        if "pomFile" in template:
+            for entity in test_model.entities:
+                with open(join(f"{output_root}/", f"pom.xml"), 'w') as f:
+                    f.write(jinja_template.render(entity=entity,
+                            projectName=project_name, time=datetime.now()))
+
+        if "mainClass" in template:
+            for entity in test_model.entities:
+                with open(join(f"{output_root}{folder_structure}", f"{project_name.capitalize()}.java"), 'w') as f:
+                    f.write(jinja_template.render(
+                        projectName=project_name, pack="jsd.tim"))
+
+        if "homeController" in template:
+            for entity in test_model.entities:
+                with open(join(f"{output_root}{folder_structure}/controller", f"HomeController.java"), 'w') as f:
+                    f.write(jinja_template.render(
+                        projectName=project_name, pack="jsd.tim", time=datetime.now()))
+
+        if "navbar" in template:
+            entities = test_model.pages.entities
+            pages = test_model.pages.pagesTypePart
+            with open(join(f"{output_root}webapp/WEB-INF/jsp", f"navbar.jsp"), 'w') as f:
+                f.write(jinja_template.render(entities=entities,
+                        pages=pages, time=datetime.now()))
+
+        if "homePage" in template:
+            entities = test_model.pages.entities
+            pages = test_model.pages.pagesTypePart
+            with open(join(f"{output_root}webapp/WEB-INF/jsp", f"homePage.jsp"), 'w') as f:
+                f.write(jinja_template.render(entities=entities,
+                        pages=pages, time=datetime.now()))
+
+        if "formJspPage" in template:
+            for entity in test_model.entities:
+                shared = [
+                    x for x in test_model.shared.entities if x.name == entity.name]
+
+                if shared:
+                    with open(join(f"{output_root}webapp/WEB-INF/jsp",
+                                   f"{entity.name}Form.jsp"), 'w') as f:
+                        f.write(jinja_template.render(entity=entity,
+                                shared=test_model.shared, time=datetime.now()))
+                else:
+                    with open(join(f"{output_root}webapp/WEB-INF/jsp",
+                                   f"{entity.name}Form.jsp"), 'w') as f:
+                        f.write(jinja_template.render(
+                            entity=entity, time=datetime.now()))
+
+        if "viewJspPage" in template:
+            for entity in test_model.entities:
+                shared = [
+                    x for x in test_model.shared.entities if x.name == entity.name]
+
+                pages = test_model.pages.pagesTypePart
+
+                if shared:
+                    with open(join(f"{output_root}webapp/WEB-INF/jsp",
+                                   f"{entity.name}View.jsp"), 'w') as f:
+                        f.write(jinja_template.render(
+                            entity=entity, shared=test_model.shared, pages=pages, time=datetime.now()))
+                else:
+                    with open(join(f"{output_root}webapp/WEB-INF/jsp",
+                                   f"{entity.name}View.jsp"), 'w') as f:
+                        f.write(jinja_template.render(entity=entity,
+                                pages=pages, time=datetime.now()))
+
+        if "updateFormJspPage" in template:
+            for entity in test_model.entities:
+                shared = [
+                    x for x in test_model.shared.entities if x.name == entity.name]
+
+                pages = test_model.pages.pagesTypePart
+
+                if shared:
+                    with open(join(f"{output_root}webapp/WEB-INF/jsp",
+                                   f"{entity.name}UpdateForm.jsp"), 'w') as f:
+                        f.write(jinja_template.render(
+                            entity=entity, shared=test_model.shared, pages=pages, time=datetime.now()))
+                else:
+                    with open(join(f"{output_root}webapp/WEB-INF/jsp",
+                                   f"{entity.name}UpdateForm.jsp"), 'w') as f:
+                        f.write(jinja_template.render(entity=entity,
+                                pages=pages, time=datetime.now()))
+
+
+@generator("springr_language", "App")
+def app_generator(metamodel, model, output_path, overwrite, debug, **custom_args):
+    '''Generate full-stack Spring Boot/JSP app'''
+    try:
+        if output_path.includes("/"):
+            project_name = output_path.split("/")[0]
+        else:
+            project_name = output_path
+        app_generator(model, output_path, project_name)
+    except Exception as e:
+        print("App generator failed due to: \n\n" + e)
---
+++ springr_generator/model.py
@@ -0,0 +1,67 @@
+from __future__ import unicode_literals
+import os
+from os.path import dirname, join
+from textx import metamodel_from_file, language, generator
+from textx.export import metamodel_export, model_export
+from springr_generator.metamodel import SimpleType, AnnotationName
+
+
+def get_entity_mm():
+    metamodel_path = dirname(__file__)
+    """
+    Builds and returns a meta-model for Entity language.
+    """
+    # Built-in simple types
+    # Each model will have this simple types during reference resolving but
+    # these will not be a part of `types` list of EntityModel.
+    type_builtins = {
+        'long': SimpleType(None, 'long'),
+        'integer': SimpleType(None, 'integer'),
+        'string': SimpleType(None, 'string'),
+        'date': SimpleType(None, 'date'),
+        'bool': SimpleType(None, 'boolean'),
+        'Column': AnnotationName(None, 'Column'),
+        'ManyToMany': AnnotationName(None, 'ManyToMany'),
+        'Id': AnnotationName(None, 'Id')
+    }
+
+    entity_mm = metamodel_from_file(metamodel_path,
+                                    classes=[SimpleType, AnnotationName],
+                                    builtins=type_builtins)
+
+    return entity_mm
+
+
+def dot_model_export(model_path, output_path):
+    entity_mm = get_entity_mm()
+
+    # Export to .dot file for visualization
+    dot_folder = join(output_path)
+    if not os.path.exists(dot_folder):
+        os.mkdir(dot_folder)
+
+    print("Exporting metamodel and model to folder: " + dot_folder)
+
+    metamodel_export(entity_mm, join(dot_folder, 'metamodel.dot'))
+
+    # Build Person model from person.ent file
+    person_model = entity_mm.model_from_file(model_path)
+
+    # Export to .dot file for visualization
+    model_export(person_model, join(dot_folder, 'model.dot'))
+
+    print("Done exporting Dot")
+
+
+@language("springr_language", "*.ent")
+def springr_language():
+    '''A language for entity description'''
+    return get_entity_mm()
+
+@generator("springr_language", "Dot")
+def dot_generator(model_path, output_path):
+    '''Generating dot visualizations from SpringR grammars'''
+    try:
+        dot_model_export(model_path, output_path)
+    except Exception as e:
+        print("Dot generator failed due to: \n\n" + e)
